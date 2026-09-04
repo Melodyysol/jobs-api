719fd756c48d19e90aa55074755935b5192bcc65
Issa Abdulwaris Atere <issaabdulwarisatere@Issas-MacBook-Air.local>
2026-09-03 21:16:35 -0700

    Swagger docs added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // authorization
 const authenticatedUser = require("./middleware/authentication");
 
@@ -33,8 +37,10 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticatedUser, jobsRouter);
